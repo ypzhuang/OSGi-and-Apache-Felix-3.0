763cc179985c698bcdd91b321c571137935ce513
Zhuang, Yin Ping <yin-ping.zhuang@hp.com>
2014-09-14 20:55:34 +0800

    add log api and impl

---
+++ com.packtpub.felix.bookshelf-log-impl/src/main/java/com/packtpub/felix/bookshelf/log/impl/BookshelfLogHelperImpl.java
@@ -0,0 +1,55 @@
+package com.packtpub.felix.bookshelf.log.impl;
+
+import java.text.MessageFormat;
+
+import org.osgi.service.log.LogService;
+
+import com.packtpub.felix.bookshelf.log.api.BookshelfLogHelper;
+
+public class BookshelfLogHelperImpl implements BookshelfLogHelper {
+
+	LogService log;
+
+	@Override
+	public void debug(String pattern, Object... args) {
+		String message = MessageFormat.format(pattern, args);
+		this.log.log(LogService.LOG_DEBUG, message);
+	}
+
+	@Override
+	public void debug(String pattern, Throwable throwable, Object... args) {
+		String message = MessageFormat.format(pattern, args);
+		this.log.log(LogService.LOG_DEBUG, message);
+	}
+
+	@Override
+	public void info(String pattern, Object... args) {
+		String message = MessageFormat.format(pattern, args);
+		this.log.log(LogService.LOG_INFO, message);
+	}
+
+	@Override
+	public void warn(String pattern, Object... args) {
+		String message = MessageFormat.format(pattern, args);
+		this.log.log(LogService.LOG_WARNING, message);
+	}
+
+	@Override
+	public void warn(String pattern, Throwable throwable, Object... args) {
+		String message = MessageFormat.format(pattern, args);
+		this.log.log(LogService.LOG_WARNING, message);
+	}
+
+	@Override
+	public void error(String pattern, Object... args) {
+		String message = MessageFormat.format(pattern, args);
+		this.log.log(LogService.LOG_ERROR, message);
+	}
+
+	@Override
+	public void error(String pattern, Throwable throwable, Object... args) {
+		String message = MessageFormat.format(pattern, args);
+		this.log.log(LogService.LOG_ERROR, message);
+	}
+
+}
